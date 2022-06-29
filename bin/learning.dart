790d8dd3beda440688212b9c7473ffb9efcb28b9
KaremSouza12 <fulldevelop340@gmail.com>
2022-06-29 19:03:46 -0400

    estrutura inicial do projeto

--- bin/learning.dart
+++ bin/learning.dart
@@ -1,3 +1,7 @@
+//Estrutura inicial de um programa em dart
 void main() {
-  print('Hello world: !');
+  //função principal onde são executados os trechos de códigos
+  print('Hello world: !'); //imprimi o frase Hello world no terminal
 }
+
+//Para executar o trecho de código é necessário usar o o comando dart run no terminal
