5c36f25682f7a276c498f244f028e11072cb4a19
KaremSouza12 <fulldevelop340@gmail.com>
2022-06-30 12:51:57 -0400

    Listas

--- bin/learning.dart
+++ bin/learning.dart
@@ -1,7 +1,52 @@
-//Estrutura inicial de um programa em dart
+/*
+ Listas em Dart pode ser declaradas da seguint forma List<tipo> lista = [1,2,3]
+ ou ainda var list = [1,2,3] 
+*/
 void main() {
-  //função principal onde são executados os trechos de códigos
-  print('Hello world: !'); //imprimi o frase Hello world no terminal
-}
+  List<String> nomes = [
+    'Karem',
+    'Lucas',
+    'Maria',
+    'Sabá'
+  ]; //a lista com tipo definido
 
-//Para executar o trecho de código é necessário usar o o comando dart run no terminal
+  var info = [
+    'Larissa',
+    'Manuela',
+    'Mariazinha',
+    'Joãozinho'
+  ]; //Lista sem tipo definido,
+
+  print(nomes);
+
+  print(info);
+
+  //Funções da lista
+
+  nomes.add("Daenarys"); //adicionar ao final da lista
+
+  print(nomes);
+
+  List<String> maisNomes = ['Khal', 'Jorah', 'Jofrey', 'Milisandre'];
+
+  nomes.addAll(maisNomes); // Adiciona mais de um elemento na lista
+
+  print(nomes);
+
+  nomes.remove("Milisandre"); //Remove do Final da lista
+
+  print(nomes);
+
+  nomes.removeAt(0); //remove pelo indice
+
+  print(nomes);
+
+  var list = nomes.contains(
+      "Jofrey"); //verifica se contain um determinado elemento na lista
+
+  print(list);
+
+  print(nomes.length);
+
+  //para acessar mais funções é só colocar o nomde da lista . que o autocomplete do editor mostra mais funções.
+}
