00b5d73102f3b8aa04524ae479c4b3b38224294d
KaremSouza12 <fulldevelop340@gmail.com>
2022-07-04 20:00:51 -0400

    create abstract class

--- bin/learning.dart
+++ bin/learning.dart
@@ -1,7 +1,25 @@
-//Estrutura inicial de um programa em dart
 void main() {
-  //função principal onde são executados os trechos de códigos
-  print('Hello world: !'); //imprimi o frase Hello world no terminal
+  Jogador j = Jogador('teslaBoy', 20, 80); //instância da classe jogador
+
+  j.mostra();
 }
 
-//Para executar o trecho de código é necessário usar o o comando dart run no terminal
+abstract class Personagem {
+  //classe abstrata usada como molde pra outras classes
+  int posicaoX = 1;
+  int posicaoY = 2;
+  String nome = '';
+
+  Personagem(this.nome, this.posicaoX, this.posicaoY);
+
+  void mostra() {
+    //método da classe
+    print('$nome está na posição x $posicaoX e $posicaoY');
+  }
+}
+
+class Jogador extends Personagem {
+  //Classe jogador herda os dados da classe personagem
+  Jogador(String nome, int posicaoX, int posicaoY)
+      : super(nome, posicaoX, posicaoY);
+}
