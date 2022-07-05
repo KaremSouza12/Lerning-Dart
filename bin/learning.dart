39ee5dd54a9999e20ac977e509d0b1533edd857e
KaremSouza12 <fulldevelop340@gmail.com>
2022-07-04 20:13:33 -0400

    insert list objects

--- bin/learning.dart
+++ bin/learning.dart
@@ -1,7 +1,26 @@
 //Estrutura inicial de um programa em dart
 void main() {
-  //função principal onde são executados os trechos de códigos
-  print('Hello world: !'); //imprimi o frase Hello world no terminal
+  List<Pessoa> pessoas = [
+    Pessoa('Karem', 29),
+    Pessoa('Lucas', 27)
+  ]; //declarando uma lista de objetos
+
+  // print(pessoa[0].nome);
+
+  pessoas.forEach((element) {
+    print(element.nome);
+    print(element.idade);
+  });
+  pessoas[0].apresenta();
 }
 
-//Para executar o trecho de código é necessário usar o o comando dart run no terminal
+class Pessoa {
+  String nome = '';
+  int idade = 20;
+
+  Pessoa(this.nome, this.idade); //construtor da classe
+  void apresenta() {
+    //método da classe
+    print('Meu nome é $nome e minha idade é $idade');
+  }
+}
