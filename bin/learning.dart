39f4fad86ffc10d7e93c8288413b6072024028be
KaremSouza12 <fulldevelop340@gmail.com>
2022-07-06 19:55:49 -0400

    input and output data

--- bin/learning.dart
+++ bin/learning.dart
@@ -1,20 +1,36 @@
-//Estrutura inicial de um programa em dart
+import 'dart:io';
+
 void main() {
-  Map<int, String> numeros = Map();
-  numeros[0] = "Um";
-  numeros[1] = "dois";
-  numeros[2] = "três";
+  List fruits = ['banana', 'pineapple', 'watermelon'];
+  fruits.forEach((element) {
+    print(element);
+  });
 
-  print(numeros);
+  var mappedFruits = fruits.map((fuits) => 'I love fruits').toList();
 
-  Map<String, String> estados = Map();
-  estados['AM'] = "Amazonas";
-  estados['SP'] = "São Paulo";
-  estados['RJ'] = "Rio de Janeiro";
+  print(mappedFruits);
 
-  print(numeros);
-  print(estados.values);
-  print(estados.keys);
+  var numbers = [1, 3, 2, 5, 4];
+  print(numbers.contains(2));
+
+  var arr = numbers.sort();
+
+  var sum = numbers.reduce((curr, next) => curr + next);
+  print(sum);
+
+  print("Informe as notas");
+  print("===========================");
+  print("Informe a nota 1");
+  String? nota1 = stdin.readLineSync();
+  print("Informe a nota 2");
+  String? nota2 = stdin.readLineSync();
+  print("Informe a nota 3");
+  String? nota3 = stdin.readLineSync();
+
+  if (nota1 != null && nota2 != null && nota3 != null) {
+    double media =
+        (double.parse(nota1) + double.parse(nota2) + double.parse(nota3)) / 3;
+
+    print(media);
+  }
 }
-
-//Para executar o trecho de código é necessário usar o o comando dart run no terminal
